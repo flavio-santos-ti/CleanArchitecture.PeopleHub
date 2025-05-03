f009f22875eb4c87e291e0068da8501744f525b0
unknown <flavio.santos.ti@outlook.com>
2025-05-03 09:23:57 -0300

    ✨ Feat(PersonType): Add entity mapping for person type table.

---
+++ backend/src/PeopleHub.Infrastructure/Persistence/Mappings/PersonTypeMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PeopleHub.Domain.Entities;
+
+namespace PeopleHub.Infrastructure.Persistence.Mappings;
+
+public class PersonTypeMap : IEntityTypeConfiguration<PersonTypeEntity>
+{
+    public void Configure(EntityTypeBuilder<PersonTypeEntity> builder)
+    {
+        builder.ToTable("person_type");
+
+        builder.HasKey(e => e.Code);
+
+        builder.Property(e => e.Code)
+            .HasColumnName("code")
+            .HasColumnType("char(1)");
+
+        builder.Property(e => e.Description)
+            .IsRequired()
+            .HasMaxLength(50)
+            .HasColumnName("description");
+    }
+}
