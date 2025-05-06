61129cf2cf36afc264f74bec18fd562c87bc50a9
unknown <flavio.santos.ti@outlook.com>
2025-05-06 09:06:59 -0300

    ♻️ Refactor(LegalPersonEntity): remove Address from constructor.

--- backend/src/PeopleHub.Application/UseCases/Legal/AddLegalUseCase.cs
+++ backend/src/PeopleHub.Application/UseCases/Legal/AddLegalUseCase.cs
@@ -48,7 +48,6 @@ public class AddLegalUseCase : IAddLegalUseCase
                 cnpj,
                 request.StateRegistration,
                 request.MunicipalRegistration,
-                address,
                 phone,
                 email,
                 request.LegalRepresentativeName,
