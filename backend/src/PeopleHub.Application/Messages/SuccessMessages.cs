8830093462485057fb568d38d2d8eaa05e265c89
unknown <flavio.santos.ti@outlook.com>
2025-05-10 00:39:03 -0300

    ♻️ Refactor(SuccessMessages): add feminine and masculine variations.

--- backend/src/PeopleHub.Application/Messages/SuccessMessages.cs
+++ backend/src/PeopleHub.Application/Messages/SuccessMessages.cs
@@ -1,16 +1,16 @@
-﻿namespace PeopleHub.Application.Messages
+﻿namespace PeopleHub.Application.Messages;
+
+public static class SuccessMessages
 {
-    public static class SuccessMessages
-    {
-        public static string CreatedFeminine(string entity) => $"{entity} cadastrada com sucesso.";
-        public static string CreatedMasculine(string entity) => $"{entity} cadastrado com sucesso.";
+    public static string CreatedFeminine(string entity) => $"{entity} cadastrada com sucesso.";
+    public static string CreatedMasculine(string entity) => $"{entity} cadastrado com sucesso.";
 
-        public static string UpdatedFeminine(string entity) => $"{entity} atualizada com sucesso.";
-        public static string UpdatedMasculine(string entity) => $"{entity} atualizado com sucesso.";
+    public static string UpdatedFeminine(string entity) => $"{entity} atualizada com sucesso.";
+    public static string UpdatedMasculine(string entity) => $"{entity} atualizado com sucesso.";
 
-        public static string Removed(string entity) => $"{entity} excluída com sucesso.";
+    public static string RemovedFeminine(string entity) => $"{entity} excluída com sucesso.";
+    public static string RemovedMasculine(string entity) => $"{entity} excluído com sucesso.";
 
-        public static string RetrievedFeminine(string entity) => $"{entity} recuperada com sucesso.";
-        public static string RetrievedMasculine(string entity) => $"{entity} recuperado com sucesso.";
-    }
+    public static string RetrievedFeminine(string entity) => $"{entity} recuperada com sucesso.";
+    public static string RetrievedMasculine(string entity) => $"{entity} recuperado com sucesso.";
 }
