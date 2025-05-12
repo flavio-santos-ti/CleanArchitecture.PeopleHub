874d800ff14ddcd579984f01300ecc53bd299527
unknown <flavio.santos.ti@outlook.com>
2025-05-12 17:16:54 -0300

    ♻️ Refactor(ValidationMessages): add templates for registration checks.

---
+++ backend/src/PeopleHub.Application/Messages/ValidationMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace PeopleHub.Application.Messages;
+
+public static class ValidationMessages
+{
+    public static string AlreadyRegisteredFeminine(string entity) => $"{entity} já cadastrada.";
+    public static string AlreadyRegisteredMasculine(string entity) => $"{entity} já cadastrado.";
+}
